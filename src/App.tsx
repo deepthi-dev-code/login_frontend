34d604ba88d721d9a5d357b393788a1b8549b925
deepthi-dev-code <deepthidileep861@gmail.com>
2025-08-25 01:26:42 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Index from './page/home';
+import Login from './page/login';
 
 function App() {
   return (
     <div className="App">
-     <Index/>
+     <Login/>
     </div>
   );
 }
