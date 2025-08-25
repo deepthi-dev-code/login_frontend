a9d82440af128f5b739f5d9a0c792a3058bcbb96
deepthi-dev-code <deepthidileep861@gmail.com>
2025-08-25 23:18:28 +0530

    upp

---
+++ src/page/webpage/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+   qwertyu
+    </div>
+  );
+}
+
+export default Homepage;
